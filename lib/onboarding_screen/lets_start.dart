1fab843c367c8a970595a27451ecd6ef1d00b72e
Lisawakhidah <wakhidahlisa@gmail.com>
2024-04-30 12:18:27 +0700

    Onboarding Screen

---
+++ lib/onboarding_screen/lets_start.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_ui/home/home.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 480,
+            decoration: BoxDecoration(
+                color: Color(0xFFF0F4EF),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(150),
+                    bottomRight: Radius.circular(150))),
+            child: Image.asset("assets/image/bg.png"),
+          ),
+          SizedBox(
+            width: 284,
+            child: Text('Create Your Own Garden!',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                  color: Color(0xFF1E271A),
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ))
+
+                // TextStyle(
+                //   color: Color(0xFF1E271A),
+                //   fontSize: 45,
+                //   fontFamily: 'google',
+                //   fontWeight: FontWeight.w600,
+                //   height: 0,
+                // ),
+                ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => const HomePage()),
+              );
+            },
+            child: Container(
+              height: 52,
+              width: 200,
+              alignment: Alignment.center,
+              decoration: ShapeDecoration(
+                color: Color(0xFF475E3E),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+              child: Text(
+                'Lets Start !',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFFFCFCFD),
+                  fontSize: 18,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
