13cae81f1eef5378b619639dfff2c27b98516be6
Lisawakhidah <wakhidahlisa@gmail.com>
2024-06-26 08:38:43 +0700

    add services-menu_source.dart

---
+++ lib/services/menu_source.dart
@@ -0,0 +1,42 @@
+import 'dart:typed_data';
+
+import 'package:appwrite/models.dart';
+import '../appwrite/appwrite.dart';
+import '../models/menu.dart';
+import 'package:appwrite/appwrite.dart';
+
+class MenuSource {
+  static Client client = Client()
+      .setEndpoint(AppwriteConstants.endpoint)
+      .setProject(AppwriteConstants.projectId);
+  static Databases databases = Databases(client);
+  static Storage storages = Storage(client);
+
+  static Future<List<Menu>> getMenus() async {
+    try {
+      final response = await databases.listDocuments(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.collectionId,
+      );
+
+      return response.documents.map((Document doc) {
+        return Menu.fromMap(doc.data);
+      }).toList();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  static Future<Uint8List?> getImage(String imageId) async {
+    try {
+      final response = await storages.getFileView(
+        bucketId: AppwriteConstants.bucketId,
+        fileId: imageId,
+      );
+      return response;
+    } catch (e) {
+      print('Error fetching image: $e');
+      return null;
+    }
+  }
+}
