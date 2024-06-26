42f7a1566067355c7be52780aa77e974a4ad4095
Lisawakhidah <wakhidahlisa@gmail.com>
2024-06-26 07:49:49 +0700

    konfigurasi appwrite

---
+++ lib/appwrite/appwrite.dart
@@ -0,0 +1,7 @@
+class AppwriteConstants {
+  static const String endpoint = 'https://cloud.appwrite.io/v1'; // Contoh endpoint
+  static const String projectId = 'YOUR_PROJECT_ID'; // Ganti dengan project ID Anda
+  static const String databaseId = 'YOUR_DATABASE_ID'; // Ganti dengan database ID Anda
+  static const String collectionId = 'YOUR_COLLECTION_ID'; // Ganti dengan collection ID Anda
+  static const String bucketId = 'YOUR_BUCKET_ID'; // Ganti dengan bucket ID Anda
+}
